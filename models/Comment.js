f77b335914df27f834547841f7433626906fd061
Daniel Bowen <djbowen95@gmail.com>
2022-04-04 14:51:57 +0100

    Model for Post, Model for Comment

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true, 
+     },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sequelize,  
+})
+
+module.exports = Comment;
